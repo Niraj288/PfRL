42009e4b5a5d73ac60635aeaff9bdc9d553898aa
MZ-Makos <mmakos@smu.edu>
2020-10-12 11:10:28 -0500

    input works

--- make_inp/make_inp.py
+++ make_inp/make_inp.py
@@ -7,14 +7,20 @@ import re
 import sys
 import numpy as np
 
-FCOUNTS = [10,20]
-TRACK = [-1,5]
-BCOUNT = [-1,5]
+FCOUNTS = [10,20,50,100]
+TRACK = [-1,20,50]
+BCOUNT = [-1,20,50]
+
+#FCOUNTS = [10,20,30,40,50,80,100,150]
+#TRACK = [-1,10,20,30,50,80,100]
+#BCOUNT = [-1,10,20,30,50,80,100]
 
 def make_inp(st1,st2,i,j,k):
-	name = 'F'+str(i)+'B'+str(j)+'T'+str(k)
+	name = 'F'+str(i)+'B'+str(j)+'T'+str(k)+'sta'
 	os.system('mkdir '+name)
-	#os.system('chdir ' + name)
+	os.system('cp -r proteins ' + name)
+	os.system('cp -r errors ' + name)
+	os.system('cp -r models ' + name)
 	f = open('inp','w')
 	f.write(st1)
 	f.close()
@@ -23,18 +29,19 @@ def make_inp(st1,st2,i,j,k):
 	f2.write(st2)
 	f2.close()
 	os.system('mv run.sbatch ' +name)
-	os.system('cd ' + name)
+	os.chdir(name)
 	#os.system('chdir ' + name)
+	print (os.getcwd())
 	os.system('sbatch run.sbatch')
-	os.system('cd ../')
+	os.chdir('../')
 
 for i in FCOUNTS:
 	for j in TRACK:
 		for k in BCOUNT:
-			st1 = """MEAN_REWARD_BOUND -3.0 \n\nGAMMA  0.99\n\nBATCH_SIZE  32\n\nREPLAY_SIZE  10000\n\nLEARNING_RATE  1e-4\n\nSYNC_TARGET_FRAMES  1000\n\nREPLAY_START_SIZE  10000\n\nEPSILON_DECAY_LAST_FRAME  10**7\n\nMAX_ITER 10**9\n\nEPSILON_START  1.0\n\nEPSILON_FINAL  0.05\n\ndevice gpu\n\nHIDDEN_SIZE 256\n\nFCOUNTS """ + str(i) + '\n\n' + 'BCOUNT ' + str(j) + '\n\n' + 'TRACK ' + str(k) + '\n'
+			st1 = """DEFAULT_ENV_NAME PfRL\n\ndoSimulation 0\n\nRENDER 0\n\nMEAN_REWARD_BOUND -3.0 \n\nGAMMA  0.99\n\nBATCH_SIZE  32\n\nREPLAY_SIZE  10000\n\nLEARNING_RATE  1e-4\n\nSYNC_TARGET_FRAMES  1000\n\nREPLAY_START_SIZE  10000\n\nEPSILON_DECAY_LAST_FRAME  10**7\n\nMAX_ITER 10**9\n\nEPSILON_START  1.0\n\nEPSILON_FINAL  0.05\n\ndevice cpu\n\nHIDDEN_SIZE 256\n\nFCOUNTS """ + str(i) + '\n\n' + 'BCOUNT ' + str(j) + '\n\n' + 'TRACK ' + str(k) + '\n'
 			st2 ="""#!/bin/bash\n#SBATCH -J inp\n#SBATCH -o log_%j.out
-#SBATCH -p gpgpu-1,v100x8
-#SBATCH -c 1\n#SBATCH --mem=124G\n\nmodule load python/3\n\npython -u ../../idqn.py inp > log.txt\n\n"""
+#SBATCH -p standard-mem-m,standard-mem-l,high-mem-1,high-mem-2 
+#SBATCH -c 1\n#SBATCH --mem=124G\n\nmodule load python/3\n\npython -u /users/mmakos/scratch/Protein_folding/PfRL/idqn.py inp > log.txt\n\n"""
 			
 			make_inp(st1,st2,i,j,k)
  
